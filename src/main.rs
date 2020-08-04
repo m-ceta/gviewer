252f6ac76b6b272de8b17e49da4bfdc2324a8922
Motohiro Seta <motohiro.seta@gmail.com>
2020-08-04 11:23:53 +0900

    create sample.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate gtk;
+use gtk::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    gtk::init().expect("Failed to initialize GTK.");
+
+    let ui = include_str!("resources/window1.glade");
+    let builder = gtk::Builder::from_string(ui);
+                    
+    let window1 : gtk::Window = builder.get_object("window1").unwrap();
+    window1.connect_delete_event(move |_, _| {
+        gtk::main_quit();
+        Inhibit(false)
+    });
+
+    window1.show_all();
+                                
+    gtk::main();
 }
